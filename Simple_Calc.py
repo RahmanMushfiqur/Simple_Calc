06cbf15e546e716d8cabd1235d02ba255407a515
Mushfiqur <mushfiqur.rahman@uleth.ca>
2026-01-23 20:40:39 -0700

    Fixed minor stuff

--- Simple_Calc.py
+++ Simple_Calc.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates")
 
-@app.route('/calc', methods=["GET", "POST"])
-def calm():
+@app.route('/', methods=["GET", "POST"])
+def index():
     
     if request.method == 'GET':
         return render_template('index.html')
