d59d22cd0d1ab66754965f38c2e9e406abb0db25
Mushfiqur <mushfiqur.rahman@uleth.ca>
2026-01-27 00:41:00 -0700

    Improved with db

--- Simple_Calc.py
+++ Simple_Calc.py
@@ -1,34 +1,60 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__, template_folder="templates")
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+class Calculation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    a = db.Column(db.Integer, nullable=False)
+    b = db.Column(db.Integer, nullable=False)
+    sign = db.Column(db.String(1), nullable=False)
+    result = db.Column(db.Float, nullable=False)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Calculation {self.id}: {self.a}{self.sign}{self.b}={self.result}>"
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-    
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method == "POST":
+    if request.method == "POST":
         try:
             a = int(request.form.get('first_number'))
             b = int(request.form.get('second_number'))
-            c = request.form.get('sign')
+            sign = request.form.get('sign')
         except ValueError:
             return "Not a number, try again!", 400
-        if c == '+':
+        
+        sign = (request.form.get('sign'))
+        
+        if sign == '+':
             result = a + b
-        elif c == '-':
+        elif sign == '-':
             result = a - b
-        elif c == '*':
+        elif sign == '*':
             result = a * b
-        elif c == '/':
+        elif sign == '/':
             if b == 0:
                 return "Cant divide by zero!"
             else:
                 result = a / b
-
-        return str(result)
+        else:
+            return "Invalid sign", 400
         
-    
+        row = Calculation(a=a, b=b, sign=sign, result=result)
+        db.session.add(row)
+        db.session.commit()
+
+        history = Calculation.query.order_by(Calculation.created.desc()).limit(10).all()
+        return render_template("index.html", result=result, history=history)
+
+    history = Calculation.query.order_by(Calculation.created.desc()).limit(10).all()
+    return render_template("index.html", history=history)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', debug=True)
